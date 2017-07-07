0d7212b9fc8ff29f86232013239a28d1d2346e38
l3kov9 <l3kov.9@gmail.com>
2017-07-07 12:45:01 +0300

    Matrices lab finished

--- MatricesLab/02.SquareWithMaximumSum/SquareSum.cs
+++ MatricesLab/02.SquareWithMaximumSum/SquareSum.cs
@@ -1,11 +1,54 @@
-﻿namespace _02.SquareWithMaximumSum
+﻿using System;
+using System.Linq;
+
+namespace _02.SquareWithMaximumSum
 {
     public class SquareSum
     {
         public static void Main()
         {
-         
+            var numbers = Console.ReadLine().Split(new []{' ',','},StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var rows = numbers[0];
+            var cols = numbers[1];
+            var matrix = new int[rows][];
 
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            var maxSum = int.MinValue;
+            var maxMatrix = new int[2,2];
+
+            for (int i = 0; i < matrix.Length-1; i++)
+            {
+                for (int j = 0; j < matrix[i].Length-1; j++)
+                {
+                    var squareSum = matrix[i][j] + matrix[i][j + 1] + matrix[i + 1][j] + matrix[i + 1][j + 1];
+                    if (maxSum < squareSum)
+                    {
+                        maxSum = squareSum;
+                        maxMatrix[0, 0] = matrix[i][j];
+                        maxMatrix[0, 1] = matrix[i][j + 1];
+                        maxMatrix[1, 0] = matrix[i + 1][j];
+                        maxMatrix[1, 1] = matrix[i + 1][j + 1];
+                    }
+                }
+            }
+
+            for (int i = 0; i < maxMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < maxMatrix.GetLength(1); j++)
+                {
+                    Console.Write(maxMatrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
         }
     }
 }
--- MatricesLab/04.PascalTriangle/PascalTriangle.cs
+++ MatricesLab/04.PascalTriangle/PascalTriangle.cs
@@ -1,10 +1,38 @@
-﻿namespace _04.PascalTriangle
+﻿using System;
+
+namespace _04.PascalTriangle
 {
     public class PascalTriangle
     {
         public static void Main()
         {
+            var rows = int.Parse(Console.ReadLine());
+            var pascal = new int[rows][];
+            var cols = 2;
+            pascal[0] = new int[1];
+            pascal[0][0] = 1;
+           
+            for (int i = 1; i < pascal.Length; i++)
+            {
+                pascal[i] = new int[cols];
+                pascal[i][0] = 1;
+                pascal[i][cols - 1] = 1;
 
+                if (pascal[i - 1].Length > 1)
+                {
+                    for (int j = 1; j < cols - 1; j++)
+                    {
+                        pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+                    }
+                }
+  
+                cols ++;
+            }
+
+            for (int i = 0; i < pascal.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ",pascal[i]));
+            }
         }
     }
 }
