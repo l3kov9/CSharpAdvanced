c2f628e0c17cd3fd370685db3b6976fa5da8c9a1
l3kov9 <l3kov.9@gmail.com>
2017-07-12 02:16:11 +0300

    Streams update

---
+++ StreamsAndFilesExercises/04.CopyBinaryFile/CopyFile.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    public class CopyFile
+    {
+        public static void Main()
+        {
+            using (var source = new FileStream("image.jpg",FileMode.Open))
+            {
+                using (var destination=new FileStream("homer.jpg",FileMode.Create))
+                {
+                    var buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        var readBytes = source.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+
+                        destination.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StreamsAndFilesExercises/05.SlicingFile/SlicinfFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace _05.SlicingFile
+{
+    public class SlicinfFile
+    {
+        public static void Main()
+        {
+            var parts = int.Parse(Console.ReadLine());
+            var sourceFile = "";
+            var destinationDirectory = "";
+
+            SliceFile(sourceFile, destinationDirectory, parts);
+        }
+
+        private static void SliceFile(string sourceFile, string destinationDirectory, int parts)
+        {
+            
+        }
+    }
+}
