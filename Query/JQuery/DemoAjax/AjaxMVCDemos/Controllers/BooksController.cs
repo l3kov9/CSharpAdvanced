602844f3e72a7ef122019613bf035d7add38f590
l3kov9 <l3kov.9@gmail.com>
2018-05-27 13:45:15 +0300

    Simple mvc project with ajax

---
+++ Query/JQuery/DemoAjax/AjaxMVCDemos/Controllers/BooksController.cs
@@ -0,0 +1,86 @@
+﻿namespace AjaxMVCDemos.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Mvc;
+
+    using AjaxMVCDemos.Models;
+
+    public class BooksController : Controller
+    {
+        public const string QuotesUrl = @"http://nvp-playground.azurewebsites.net/api/quotes/random/txt";
+
+        public ActionResult Index()
+        {
+            var data = BooksData
+                .GetAll()
+                .AsQueryable()
+                .Select(BookViewModel.FromBook)
+                .ToList();
+
+            return this.View(data);
+        }
+
+        public ActionResult Search(string query)
+        {
+            var result = BooksData
+                .GetAll()
+                .AsQueryable()
+                .Where(book => book.Title.ToLower().Contains(query.ToLower()))
+                .Select(BookViewModel.FromBook)
+                .ToList();
+
+            return this.PartialView("_BookResult", result);
+        }
+
+        public ActionResult ContentById(int id)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return this.Content("This action can be invoke only by AJAX call");
+            }
+
+            var book = BooksData.GetAll().FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return this.Content("Book not found");
+            }
+
+            return this.Content(book.Content);
+        }
+
+        public ActionResult All()
+        {
+            return this.View();
+        }
+
+        public JsonResult AllBooks()
+        {
+            var books = BooksData.GetAll()
+                .AsQueryable().Select(TitledBookViewModel.FromBook);
+
+            var client = new WebClient();
+            var quoteAsText = client.DownloadString(QuotesUrl);
+
+            var quoteLines = quoteAsText.Split(new[] { Environment.NewLine },StringSplitOptions.None);
+
+            var quote = new Quote()
+            {
+                Text = quoteLines[0].Replace('’','*'),
+                Author = quoteLines[1]
+            };
+
+            return this.Json(quote, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Quote
+    {
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+    }
+}
---
+++ Query/JQuery/DemoAjax/AjaxMVCDemos/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿namespace AjaxMVCDemos.Controllers
+{
+    using AjaxMVCDemos.Models;
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public const string QuotesUrl = @"http://nvp-playground.azurewebsites.net/api/quotes/random/txt";
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult ServerTime()
+        {
+            //var isAjax = Request.IsAjaxRequest();
+            
+            
+            Thread.Sleep(2000);
+            return this.Content(  " " + DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult JQueryAjax()
+        {
+            return this.View();
+        }
+
+        public ActionResult AjaxActionLink()
+        {
+            return this.View();
+        }
+
+        public ActionResult Quote()
+        {
+            return View();
+        }
+
+        public ActionResult GetQuote()
+        {
+            Thread.Sleep(2000);
+
+            var client = new WebClient();
+            var quoteAsText = client.DownloadString(QuotesUrl);
+
+            var quoteLines = quoteAsText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var quote = new RndQuote()
+            {
+                Text = quoteLines[0].Replace('’', '*'),
+                Author = quoteLines[1]
+            };
+
+            return this.Content(quote.Text + "<br/>" + quote.Author);
+        }
+        public JsonResult AllBooks()
+        {
+            var books = BooksData.GetAll()
+                .AsQueryable().Select(TitledBookViewModel.FromBook);
+
+            var client = new WebClient();
+            var quoteAsText = client.DownloadString("http://nvp-playground.azurewebsites.net/api/quotes/random/txt");
+
+            var quoteLines = quoteAsText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var quote = new Quote()
+            {
+                Text = quoteLines[0],
+                Author = quoteLines[1]
+            };
+
+            return this.Json(quote, JsonRequestBehavior.AllowGet);
+        }
+    }
+    public class RndQuote
+    {
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("“{0}“, by {1}", this.Text, this.Author);
+        }
+    }
+}
